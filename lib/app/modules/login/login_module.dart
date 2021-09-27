51616e7dfca59972eff0268988885364ac5289cc
Tiago <tiago433@hotmail.com>
2021-09-27 09:03:07 -0300

    alterando o login module para melhorar a navegação de paginas nomeadas

--- lib/app/modules/login/login_module.dart
+++ lib/app/modules/login/login_module.dart
@@ -2,14 +2,11 @@ import 'package:recuperaposte/app/modules/login/login_Page.dart';
 import 'package:recuperaposte/app/modules/login/login_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:recuperaposte/app/modules/signup/signup_module.dart';
-import 'package:recuperaposte/app/modules/signup/signup_page.dart';
-import 'package:recuperaposte/app/modules/signup/signup_store.dart';
 
 class LoginModule extends Module {
   @override
   final List<Bind> binds = [
     Bind.lazySingleton((i) => LoginStore()),
-    Bind.lazySingleton((i) => SignupStore()),
   ];
 
   @override
